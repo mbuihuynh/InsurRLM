44d2dc5a4b6f2f7876020f8cc4efc934f8df1266
Mai Bui <mai.bui@smartpayvn.com>
2025-12-18 17:03:02 +0700

    18Dec2025

---
+++ configs/cf_knotbase.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class KnotBase:
+
+    LOCATION=":memory:"
+    COLL_NAME="services"
+    EMB_DIM=1024
+
+    
--- services/knotTools/knotBase.py
+++ services/knotTools/knotBase.py
@@ -1,8 +1,18 @@
+"""
+    https://doc.agentscope.io/tutorial/task_rag.html
+"""
+
 from utils.CBase import CBase
 from utils.LoggingUtils import MessageTemplate
 
 from dataclasses import dataclass
 import os
+
+from agentscope.rag import SimpleKnowledge, QdrantStore, TextReader
+from agentscope.embedding import DashScopeTextEmbedding
+
+from configs.cf_knotbase import KnotBase
+
 
 
 class KNOTBase(CBase):
@@ -10,7 +20,7 @@ class KNOTBase(CBase):
         KNOTBase develop a modular RAG
     """
 
-    def __init__(self,knot:dataclass, reasoning=True):
+    def __init__(self,knot:dataclass=KnotBase(), reasoning=True):
         super().__init__()
 
         self.knot = knot
@@ -18,6 +28,18 @@ class KNOTBase(CBase):
 
     def _init_(self):
         self.model = None
+
+        self.knotBase = SimpleKnowledge(
+            embedding_store=QdrantStore(
+                location=self.knot.LOCATION,
+                collection_name=self.knot.COLL_NAME,
+                dimensions=self.knot.EMB_DIM
+            ),
+            embedding_model=DashScopeTextEmbedding(
+
+            )
+        )
+
 
     def tool(self):
         output = None
@@ -28,4 +50,4 @@ class KNOTBase(CBase):
 if __name__ == "__main__":
     print("Hello KNOTBase Agent Template")
 
-    
+
